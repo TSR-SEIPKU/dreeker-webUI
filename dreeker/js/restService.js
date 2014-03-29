d55fa025a252c4769b391f8d10ee4e67f17429da
Lingxiao Zhang <operaphantom.zhang@gmail.com>
2014-03-29 20:10:28 +0800

    restService support passing closure to call back

--- dreeker/js/restService.js
+++ dreeker/js/restService.js
@@ -5,11 +5,16 @@
 */
 angular.module('dreeker.restAPI', ['ngResource']).
 factory('restService', ['$resource', function($resource){
-	return function(serviceName, params, back){
+	return function(serviceName, params, back, callbackObj){
 		var data = $resource(serviceName, params).get(function(){
 			// alert(angular.toJson(data, true));
 			if(data.result == 'suc') {
-				back(data.object);
+				if (callbackObj == null) {
+					back(data.object);
+				} else {
+					//用apply传递closure
+					back.apply(callbackObj, [data.object]);
+				}
 			} else if (data.result == 'login') {
 
 			} else {
